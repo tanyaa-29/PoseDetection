803a90c0811e0e3db13ec7a2d28568d8a45fe65d
tanyaa-29 <tanyaadahiya0@gmail.com>
2025-06-01 13:49:35 +0530

    adding ui & changing the button to cardview

--- app/src/main/java/com/example/yogaimagepose/MainActivity.kt
+++ app/src/main/java/com/example/yogaimagepose/MainActivity.kt
@@ -1,42 +1,65 @@
 package com.example.yogaimagepose
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.Button
+import android.util.Log
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.IOException
+import androidx.cardview.widget.CardView
+
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var imageView: ImageView
-    private lateinit var gallerybtn: Button
-    private lateinit var camerabtn: Button
+    var galleryCard: CardView? = null
+    var cameraCard: CardView? = null
+    var imageView: ImageView? = null
+    var uri: Uri? = null
+    val permissionCode = 100
 
-    val galleryActivityResultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult(), ActivityResultCallback
-        {
-            if (it.resultCode === RESULT_OK) {
-                val image_uri: Uri? = it.data?.data
-                imageView.setImageURI(image_uri)
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val galleryActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback { result ->
+            if (result.resultCode == RESULT_OK) {
+                uri = result.data!!.data
+                val inputImage = uriToBitmap(uri!!)
+                val rotated = rotateBitmap(inputImage)
+                imageView!!.setImageBitmap(rotated)
             }
-        }
-    )
+        })
 
+    // Camera Activity Result
+    private val cameraActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback { result ->
+            if (result.resultCode == RESULT_OK) {
+                val inputImage = uriToBitmap(uri!!)
+                val rotated = rotateBitmap(inputImage)
+                imageView!!.setImageBitmap(rotated)
+            }
+        })
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -47,76 +70,103 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Initialize views
+        galleryCard = findViewById(R.id.galleryCard)
+        cameraCard = findViewById(R.id.cameraCard)
         imageView = findViewById(R.id.imageView)
-        gallerybtn = findViewById(R.id.button)
-        camerabtn = findViewById(R.id.button2)
 
-        gallerybtn.setOnClickListener {
+        // Check and request permissions
+        checkAndRequestPermissions()
+
+        // Pick from gallery
+        galleryCard!!.setOnClickListener {
             val galleryIntent =
                 Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             galleryActivityResultLauncher.launch(galleryIntent)
-
         }
-        checkAndRequestPermissions();
-
-        //capture from camera
-        camerabtn !!.setOnClickListener{
+        // Capture from camera
+        cameraCard!!.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
-                PackageManager.PERMISSION_GRANTED            ) {
-                openCamera()
-            } else {
-                requestPermissions(arrayOf(Manifest.permission.CAMERA),1211)
-            }
-        }
-    }
-        private fun checkAndRequestPermissions() {
-            val permissionsToRequest = mutableListOf<String>()
-
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) !=
                 PackageManager.PERMISSION_GRANTED
             ) {
-                permissionsToRequest.add(android.Manifest.permission.CAMERA)
-            }
-            //Android 13+ requires READ_MEDIA_IMAGE instead of READ_EXTERNAL_STORAGE
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                if (ContextCompat.checkSelfPermission(
-                        this,
-                        android.Manifest.permission.READ_MEDIA_IMAGES
-                    ) !=
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-                    permissionsToRequest.add(android.Manifest.permission.READ_MEDIA_IMAGES)
-                }
+                openCamera()
             } else {
-                if (ContextCompat.checkSelfPermission(
-                        this,
-                        android.Manifest.permission.READ_EXTERNAL_STORAGE
-                    ) !=
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-                    permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-                }
-            }
-            if (permissionsToRequest.isNotEmpty()) {
-                requestPermissions(permissionsToRequest.toTypedArray(), 1211)
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), permissionCode)
             }
         }
-    var image_uri : Uri? = null
-    private fun openCamera(){
+    }
+
+    private fun checkAndRequestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsToRequest.add(Manifest.permission.CAMERA)
+        }
+        // Android 13+ requires READ_MEDIA_IMAGES instead of READ_EXTERNAL_STORAGE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.READ_MEDIA_IMAGES)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            requestPermissions(permissionsToRequest.toTypedArray(), permissionCode)
+        }
+    }
+
+    private fun openCamera() {
         val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE,"New Picture")
-        values.put(MediaStore.Images.Media.DESCRIPTION,"From  the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
-        val cameraIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri)
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
         cameraActivityResultLauncher.launch(cameraIntent)
     }
-    //camera activity result
-    private val cameraActivityResultLauncher= registerForActivityResult (
-        ActivityResultContracts.StartActivityForResult(),
-        ActivityResultCallback { it ->
-       imageView.setImageURI(image_uri)
-        })
+
+    private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
+        return try {
+            val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedFileUri, "r")
+            val fileDescriptor = parcelFileDescriptor!!.fileDescriptor
+            val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+            parcelFileDescriptor.close()
+            image
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 
-
+    @SuppressLint("Range")
+    fun rotateBitmap(input: Bitmap?): Bitmap {
+        val orientationColumn = arrayOf(MediaStore.Images.Media.ORIENTATION)
+        val cur = contentResolver.query(uri!!, orientationColumn, null, null, null)
+        var orientation = -1
+        if (cur != null && cur.moveToFirst()) {
+            orientation = cur.getInt(cur.getColumnIndex(orientationColumn[0]))
+            cur.close()
+        }
+        Log.d("tryOrientation", orientation.toString() + "")
+        val rotationMatrix = Matrix()
+        rotationMatrix.setRotate(orientation.toFloat())
+        return Bitmap.createBitmap(
+            input!!,
+            0,
+            0,
+            input.width,
+            input.height,
+            rotationMatrix,
+            true
+        )
+    }
+}
